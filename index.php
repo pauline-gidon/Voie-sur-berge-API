fcf7aab55290f05432d9fada9fca8217d96bab16
pauline gidon <pauline.gidon@brassart.fr>
2021-10-18 13:01:07 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>Voie sur berge</title>
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+    <!-- lien font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Sora&display=swap" rel="stylesheet"> 
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Galada&display=swap" rel="stylesheet"> 
+<!-- lien favicon -->
+<link rel="icon" type="image/png" href="images/car-icone.png" />
+</head>
+<body>
+
+    <h1>Voie sur berge</h1>
+
+    <div id="alerte"></div>
+    <div class="resultat" id="result"></div>
+    
+    <footer><p><a href="https://metromobilite.fr" rel="nofollow">source des données</a></p></footer>
+    
+    <script src="js/api-vsb" defer></script>  
+</body>
+</html>
